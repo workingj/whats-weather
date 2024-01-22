20343c95a773f80be6b02b676b39d1bbe9d99f8f
jb <workingj@pm.me>
2024-01-22 10:21:01 +0100

    initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <main>
       <h1>Whatsweather</h1>
-    </>
-  )
+      <div id="main" className="c">
+        main
+      </div>
+      <div id="forecast" className="c">
+        forcast
+      </div>
+      <div id="text" className="c">
+        <p>
+          <b>Q:</b>How do hurricanes see? <br /> <b>A:</b> With one eye!
+        </p>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
