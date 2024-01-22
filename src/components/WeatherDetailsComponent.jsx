b2629be43e965b26108609efa508f34613f379ed
John Schulte <johannes-schulte@gmx.de>
2024-01-22 16:43:39 +0100

    Refactor UI layout and update weather location

--- src/components/WeatherDetailsComponent.jsx
+++ src/components/WeatherDetailsComponent.jsx
@@ -5,12 +5,14 @@ function WeatherDetailsComponent() {
   const { weatherData } = useContext(DataContext);
 
   const tempCelsius = weatherData.main.temp - 273.15;
+  const currentLocation = weatherData.name;
   const weatherDescription = weatherData.weather[0].description; //
   const date = new Date();
   const currentDate = date.toLocaleDateString();
   const currentTime = date.toLocaleTimeString();
   return (
     <div className="detail">
+      <h4>Location: {currentLocation}</h4>
       <h4>Temperature: {tempCelsius.toFixed(2)} °C</h4>
       <h4>Weather: {weatherDescription}</h4>
       <h4>Date: {currentDate}</h4>
